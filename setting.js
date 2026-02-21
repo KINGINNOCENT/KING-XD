02faae0e93246f43f4e42f41f2f9ab24073d47f6
KING INNOCENT <kinginnocent609@gmail.com>
2026-02-21 17:07:33 +0200

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,10 @@
+//=====💗 ᴋɪɴɢ ɪɴɴᴏᴄᴇɴᴛ xᴅ 💗====
+
+module.exports = {
+  BOT_NAME: 'ᴋɪɴɢ ɪɴɴᴏᴄᴇɴᴛ  xᴅ', // ur bot name
+  OWNER_NAME: 'ᴋɪɴɢ ɪɴɴᴏᴄᴇɴᴛ', //your name
+  OWNER_NUMBER: '27765456219', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Harare' // ur timezone
+};
